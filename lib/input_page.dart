47ee0a238e896fb5c648a6f61cdf12c46a6f040e
afpaiva <andrepaiva-arq@gmail.com>
2022-01-15 22:47:17 -0300

    first screen layout

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'reusableCard.dart';
+
+const Color CARD_BACKGROUND_COLOR = Color(0xFF1D1E33);
+const Color ACCENT_COLOR = Color(0xFFEB1555);
+const double BOTTOM_BAR_HEIGHT = 80.0;
 
 class InputPage extends StatefulWidget {
-  const InputPage({Key? key}) : super(key: key);
-
   @override
   State<InputPage> createState() => _InputPageState();
 }
@@ -14,8 +17,51 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Center(
-        child: Text("hello world"),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  cardColor: CARD_BACKGROUND_COLOR,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  cardColor: CARD_BACKGROUND_COLOR,
+                ),
+              )
+            ],
+          )),
+          Expanded(
+            child: ReusableCard(
+              cardColor: CARD_BACKGROUND_COLOR,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: CARD_BACKGROUND_COLOR,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: CARD_BACKGROUND_COLOR,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: ACCENT_COLOR,
+            height: BOTTOM_BAR_HEIGHT,
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 10.0),
+          )
+        ],
       ),
     );
   }
